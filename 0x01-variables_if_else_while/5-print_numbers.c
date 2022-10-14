483d559503d5f021a34062e3c02e8126024b9dae
abikifle123 <abigailkifle89@gmail.com>
2022-10-14 23:12:25 +0300

    hots

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+ * main - Here goes nothing.
+ *
+ * Return: This is fun.
+ */
+int main(void)
+{
+
+int num = 0;
+
+while (num < 10)
+{
+printf("%d", num);
+num++;
+}
+
+putchar('\n');
+
+return (0);
+}
