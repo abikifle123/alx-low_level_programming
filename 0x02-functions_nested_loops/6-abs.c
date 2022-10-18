d178d236b1a0785af5e5e09d5fd0cd0cdd1ab52b
abikifle123 <abigailkifle89@gmail.com>
2022-10-18 21:56:04 +0300

    lpio

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+#include <math.h>
+#include "main.h"
+
+/**
+ * _abs - Here lies the function.
+ *@iNumber: SERENITY IS PEACE
+ *
+ * Return: This is bullshit
+ */
+int _abs(int iNumber)
+{
+
+double dNumber = (double) iNumber;
+
+double rawResult = fabs(dNumber);
+
+int result = (int) rawResult;
+
+return (result);
+
+}
