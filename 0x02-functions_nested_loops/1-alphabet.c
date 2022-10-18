c3ea4faaf4fe32de451b68aba30b032735ffb470
abikifle123 <abigailkifle89@gmail.com>
2022-10-18 21:43:52 +0300

    moree

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,25 +1,20 @@
 #include "main.h"
 
 /**
- * print_alphabet_x10 - Here lies the function.
+ * print_alphabet - Here lies the function.
  *
- * Return: This is fun.
+ * Return: This is fun
  */
-void print_alphabet_x10(void)
+void print_alphabet(void)
 {
 
 char *output = "abcdefghijklmnopqrstuvwxyz";
-int n = 0;
-while (n < 10)
-{
 int i = 0;
 while (i < 26)
 {
 _putchar(output[i]);
 i++;
 }
-_putchar('\n');
-n++;
-}
 
+_putchar('\n');
 }
